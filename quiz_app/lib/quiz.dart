c9cb8ae46417a7741ca7a7b90926e09e77931656
Natanael Afonso <nagonso7@gmail.com>
2024-07-11 22:27:59 -0300

    S03 - Support change screen using IF

--- quiz_app/lib/quiz.dart
+++ quiz_app/lib/quiz.dart
@@ -7,27 +7,36 @@ class Quiz extends StatefulWidget {
 
   @override
   State<StatefulWidget> createState() {
-      return _QuizState();
+    return _QuizState();
   }
 }
 
 class _QuizState extends State<Quiz> {
-  Widget? activeScreen;
+  // Widget? activeScreen;
 
-  @override
-  void initState() {
-     activeScreen = StartScreen(switchScreen);
-    super.initState();
-  }
+  // @override
+  // void initState() {
+  //    activeScreen = StartScreen(switchScreen);
+  //   super.initState();
+  // }
+  var activeScreen = 'start-screen';
 
   void switchScreen() {
     setState(() {
-      activeScreen = const QuestionsScreen();
+      //activeScreen = const QuestionsScreen();
+      activeScreen = 'questions-screen';
     });
   }
 
   @override
   Widget build(BuildContext context) {
+
+    Widget screenWidget = StartScreen(switchScreen);
+
+    if (activeScreen == 'questions-screen') {
+      screenWidget = const QuestionsScreen();
+    }
+
     return MaterialApp(
       home: Scaffold(
         body: Container(
@@ -41,11 +50,12 @@ class _QuizState extends State<Quiz> {
               end: Alignment.bottomRight,
             ),
           ),
-          child: activeScreen,
+          child: screenWidget,
+          // child: activeScreen == 'start-screen'
+          //     ? StartScreen(switchScreen)
+          //     : const QuestionsScreen(),
         ),
       ),
     );
   }
-  
-  
 }
