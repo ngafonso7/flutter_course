aec38a69afb604102d0c6396b623c667ab5848d5
Natanael Afonso <nagonso7@gmail.com>
2024-07-11 22:11:54 -0300

    Initial project Quiz App

---
+++ quiz_app/lib/quiz.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/questions_screen.dart';
+import 'package:quiz_app/start_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+      return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  Widget? activeScreen;
+
+  @override
+  void initState() {
+     activeScreen = StartScreen(switchScreen);
+    super.initState();
+  }
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = const QuestionsScreen();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 61, 22, 151),
+                Color.fromARGB(255, 88, 40, 199),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: activeScreen,
+        ),
+      ),
+    );
+  }
+  
+  
+}
